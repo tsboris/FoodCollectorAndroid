b84a302eb51a7c910cb678e014bb1ab657dcf84b
ColonelKassad <Alexander.Haritonov@gmail.com>
2015-08-28 21:31:23 +0300

    myxPublication screen

--- app/src/main/java/upp/foodonet/AllPublicationsActivity.java
+++ app/src/main/java/upp/foodonet/AllPublicationsActivity.java
@@ -1,6 +1,9 @@
 package upp.foodonet;
 
+import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.database.CharArrayBuffer;
 import android.database.ContentObserver;
 import android.database.Cursor;
@@ -48,6 +51,8 @@ public class AllPublicationsActivity
         loaderCallback = new CursorLoaderCallback(this, adapter);
         getLoaderManager().initLoader(0, null, loaderCallback);
 
+        final Context context = this;
+
         // this is for user to be able to click on list elements
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -55,17 +60,24 @@ public class AllPublicationsActivity
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                // ListView Clicked item index
-                int itemPosition     = position;
-
                 // ListView Clicked item value
-                String  itemValue    = listView.getItemAtPosition(position).toString();
+                Object item = listView.getItemAtPosition(position);
+                try {
+                    FCPublication pub = (FCPublication) item;
+                    int pubId = pub.getUniqueId();
+                    Log.i(MY_TAG, "item at Position :" + position + "  cklicked " + "Id = " + Integer.toString(pubId));
+                    Bundle bundle = new Bundle();
+                    bundle.putInt(FCPublication.PUBLICATION_UNIQUE_ID_KEY, pubId);
+                    bundle.putCharSequence(FCPublication.PUBLICATION_TITLE_KEY, pub.getTitle());
+                    bundle.putCharSequence(FCPublication.PUBLICATION_SUBTITLE_KEY, pub.getSubtitle());
 
-                // Show Alert
-                Toast.makeText(getApplicationContext(),
-                        MY_TAG + " Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
-                        .show();
+                    Intent allPublicationsIntent = new Intent(context, MyPublicationDetailsActivity.class);
+                    startActivity(allPublicationsIntent);
 
+                } catch (Exception ex) {
+                    Log.e(MY_TAG, "item at Position :" + position + "  cklicked. " + " Item is not FCPublication!");
+                    return;
+                }
             }
 
         });
