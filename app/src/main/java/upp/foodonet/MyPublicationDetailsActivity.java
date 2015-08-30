ffeac92c3455a2f9ac982dff296cc3a86460d6d1
ColonelKassad <Alexander.Haritonov@gmail.com>
2015-08-30 03:38:57 +0300

    removed request to database. assuming FCPublication is passed as 'extras'

--- app/src/main/java/upp/foodonet/MyPublicationDetailsActivity.java
+++ app/src/main/java/upp/foodonet/MyPublicationDetailsActivity.java
@@ -2,6 +2,7 @@ package upp.foodonet;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -19,7 +20,6 @@ import DataModel.FCPublication;
 import FooDoNetServiceUtil.FooDoNetCustomActivityConnectedToService;
 
 public class MyPublicationDetailsActivity extends FooDoNetCustomActivityConnectedToService
-    implements LoaderManager.LoaderCallbacks<Cursor>
 {
     private final String MY_TAG = "MyPublicationDetailsActivity";
 
@@ -29,18 +29,28 @@ public class MyPublicationDetailsActivity extends FooDoNetCustomActivityConnecte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_publication_details);
 
-        publicationID = savedInstanceState.getInt(FCPublication.PUBLICATION_UNIQUE_ID_KEY);
-        String publicationTitle = savedInstanceState.getString(FCPublication.PUBLICATION_TITLE_KEY);
-        String publicationSubtitle = savedInstanceState.getString(FCPublication.PUBLICATION_SUBTITLE_KEY);
+        try {
+            Intent i = getIntent();
+            this.publication = (FCPublication) i.getSerializableExtra("publication");
+        } catch (Exception ex) {
+            Log.e(MY_TAG, "error deserializing passed FCPublication: " + ex.getMessage());
+            return;
+        }
 
-        if (publicationTitle!=null)  setTitle(publicationTitle);
-
-        subtitleTextView = (TextView) findViewById(R.id.tv_subtitle);
-        subtitleTextView.setText(publicationSubtitle);
+        if (publication.getTitle() != null) {
+            this.setTitle(publication.getTitle());
+            subtitleTextView = (TextView) findViewById(R.id.tv_subtitle);
+            subtitleTextView.setText(publication.getTitle());
+        }
 
         interestedPersonsCountTextView = (TextView) findViewById(R.id.interested_persons_count);
         postAddressTextView =            (TextView) findViewById(R.id.post_address);
         publicationDescriptionTextView = (TextView) findViewById(R.id.publication_description);
+
+        interestedPersonsCountTextView.setText(R.string.going_to_collect + "  "
+                + Integer.toString(publication.getRegisteredForThisPublication().size()));
+        postAddressTextView.setText(publication.getAddress());
+        publicationDescriptionTextView.setText(publication.getSubtitle());
 
         cancelPublicationButton        = (Button) findViewById(R.id.btn_cancel_publication);
         cancelPublicationButton.setText(R.string.cancel_publication);
@@ -100,7 +110,7 @@ public class MyPublicationDetailsActivity extends FooDoNetCustomActivityConnecte
     }
     //endregion
 
-    private int publicationID;
+    private FCPublication publication;
 
     private TextView subtitleTextView;
     private TextView interestedPersonsCountTextView;
@@ -108,45 +118,4 @@ public class MyPublicationDetailsActivity extends FooDoNetCustomActivityConnecte
     private TextView publicationDescriptionTextView;
     private Button cancelPublicationButton;
 
-
-    //region LoaderManager.LoaderCallbacks
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Log.i(MY_TAG, " onCreateLoader");
-
-        String[] projection = FCPublication.GetColumnNamesArray();
-        CursorLoader cursorLoader = new CursorLoader(this,
-                FooDoNetSQLProvider.CONTENT_URI,
-                projection,
-               FCPublication.PUBLICATION_UNIQUE_ID_KEY + " = ?",
-                new String[] {Integer.toString(publicationID)},
-                null);
-
-        return cursorLoader;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        Log.i(MY_TAG, " onLoadFinished");
-        ArrayList<FCPublication> pubArr = FCPublication.GetArrayListOfPublicationsFromCursor(data);
-        if (pubArr == null || pubArr.size() > 1 || pubArr.size() == 0 ) {
-            Log.e(MY_TAG, "error getting data for publication " + Integer.toString(publicationID));
-            return;
-        }
-
-        FCPublication pub = pubArr.get(0);
-
-        setTitle(pub.getTitle());
-        subtitleTextView.setText(pub.getSubtitle());
-        interestedPersonsCountTextView.setText(R.string.going_to_collect + "  "
-                + Integer.toString(pub.getRegisteredForThisPublication().size()));
-        postAddressTextView.setText(pub.getAddress());
-        publicationDescriptionTextView.setText(pub.getSubtitle());
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-    //endregion
 }
