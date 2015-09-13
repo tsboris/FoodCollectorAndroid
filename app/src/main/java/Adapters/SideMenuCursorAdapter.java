229a095cdbe9db3f48680d9ef45b42484fdf5f6a
asher-rulez <asher.rulez@gmail.com>
2015-09-13 18:23:58 +0300

    only added new adapter class, no integration

---
+++ app/src/main/java/Adapters/SideMenuCursorAdapter.java
@@ -0,0 +1,49 @@
+package Adapters;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import upp.foodonet.R;
+
+/**
+ * Created by Asher on 13.09.2015.
+ */
+public class SideMenuCursorAdapter extends CursorAdapter {
+
+    public SideMenuCursorAdapter(Context context, Cursor cursor, int flags) {
+        super(context, cursor, 0);
+    }
+
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;//LayoutInflater.from(context).inflate(R.layout.item_todo, parent, false);
+    }
+
+    // The bindView method is used to bind all data to a given view
+    // such as setting the text on a TextView.
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find fields to populate in inflated template
+/*
+        TextView tvBody = (TextView) view.findViewById(R.id.tvBody);
+        TextView tvPriority = (TextView) view.findViewById(R.id.tvPriority);
+*/
+        // Extract properties from cursor
+/*
+        String body = cursor.getString(cursor.getColumnIndexOrThrow("body"));
+        int priority = cursor.getInt(cursor.getColumnIndexOrThrow("priority"));
+*/
+        // Populate fields with extracted properties
+/*
+        tvBody.setText(body);
+        tvPriority.setText(String.valueOf(priority));
+*/
+    }
+}
