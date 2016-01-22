fe0c341fb153e32eeb9f87c223eb4f3bf0dad905
asher.rulez <asher.rulez@gmail.com>
2016-01-22 23:39:06 +0200

    close application on back pressed

--- app/src/main/java/upp/foodonet/MapAndListActivity.java
+++ app/src/main/java/upp/foodonet/MapAndListActivity.java
@@ -385,7 +385,11 @@ public class MapAndListActivity
     }
 
     private void ForceReturn(){
-        finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        //finish();
     }
 
     @Override
--- app/src/main/java/upp/foodonet/MyPublicationsActivity.java
+++ app/src/main/java/upp/foodonet/MyPublicationsActivity.java
@@ -213,7 +213,11 @@ public class MyPublicationsActivity
     }
 
     private void ForceReturn(){
-        finish();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        //finish();
     }
 
     @Override
